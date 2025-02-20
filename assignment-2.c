4f9f5e136b1e59abf6dd40e90ae732e8b3915a4f
rasit345 <rasit3915@gmail.com>
2025-02-20 16:10:04 +0530

    Create assignment-2.c

---
+++ assignment-2.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	// Area of circle
+//	float r, area;
+//	printf("Enter the radius of the circle: ");
+//	scanf("%f", &r);
+//	
+//	area = M_PI * pow (r, 2);
+//	
+//	printf("The area of the circle is %.2f", area);
+	
+	// Simple interest
+//	float p, n, r;
+//	
+//	printf("Enter the principle amount: ");
+//	scanf("%f", &p);
+//	printf("Enter the time period (in years): ");
+//	scanf("%f", &n);
+//	printf("Enter the rate of interest: ");
+//	scanf("%f", &r);
+//	
+//	float simple_interest = (p * n * r) / 100;
+//	
+//	printf("The simple interest is %.2f", simple_interest);
+	
+	
+	// Area of rectangle
+//	float l, b;
+//	
+//	printf("Enter the length and breadth respectively (space-separated): ");
+//	scanf("%f %f", &l, &b);
+//	
+//	float area = l * b;
+//	
+//	printf("The area of the rectangle is %.2f", area);
+	
+	// Temperature conversion
+	
+	// 1. Celsius to Fahrenheit
+//	float temp_in_c;
+//	
+//	printf("Enter the temperature in celsius: ");
+//	scanf("%f", &temp_in_c);
+//	
+//	float temp_in_fh = ((float) 9 / 5) * temp_in_c + 32;
+//	
+//	printf("The temperature in fahrenheit is %.2f", temp_in_fh);
+	
+	// 2. Fahrenheit to celsius
+//	float temp_in_fh;
+//	
+//	printf("Enter the temperature in celsius: ");
+//	scanf("%f", &temp_in_fh);
+//	
+//	float temp_in_c = (temp_in_fh - 32) * (float) 5 / 9;
+//	
+//	printf("The temperature in fahrenheit is %.2f", temp_in_c);
+	
+	// Simple calculator
+	
+//	float a, b;
+//	
+//	printf("Enter a and b respectively (space-separated): ");
+//	scanf("%f %f", &a, &b);
+//	
+//	char op;
+//	printf("Enter the mathematical operator (+ / - / * / /): ");
+//	scanf(" %c", &op);
+//	
+//	float result;
+//	if (op == '+') {
+//		result = a + b;
+//	}
+//	else if (op == '-') {
+//		result = a - b;
+//	}
+//	else if (op == '*') {
+//		result = a * b;
+//	}
+//	else if (op == '/') {
+//		if (b == 0) {
+//			printf("Error: Division by zero");
+//			return 1;
+//		}
+//		else {
+//			result = a / b;
+//		}
+//	}
+//	else {
+//		printf("Invalid operation");
+//	}
+//	
+//	
+//	printf("Result: %.2f", result);
+	
+	
+	return 0;
+}
