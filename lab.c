ef4361fcccb8c3b45b4423e70f5716129030b0e6
rasit345 <rasit3915@gmail.com>
2025-02-20 15:21:59 +0530

    final lab.c

--- lab.c
+++ lab.c
@@ -1,57 +1,58 @@
 #include <stdio.h>
 #include <math.h>
 
-int sum (int, int, int);
+float sum(float, float, float);
+int is_armstrong_num(int);
 
 int main() {
 	// 1
 //	printf("Hello, World!\n");
 	
+	
 	// 2
 //	int a;
 //	printf("Enter the number: ");
 //	scanf("%d", &a);
+//	printf("The last digit of the number is %d\n", a % 10);
 //	
-//	int l = a % 10;
 //	a /= 10;
-//	
-//	printf("The last digit of the number: %d\n", l);
-//	printf("The number after removing the last digit: %d", a);
-
+//	printf("The number after removing the last digit is %d", a);
+	
+	
 	// 3
-//	int a;
+//	int a, b, c;
 //	printf("Enter the number: ");
 //	scanf("%d", &a);
-//	
-//	int l = a / 100;
-//	a -= (l * 100);
-//	printf("The last two digits: %d", a);
+//	b = a;
+//	a /= 10;
+//	c = b - 10 * a;
+//	printf("The last digit of the number is %d\n", c);
 	
+
 	// 4
 //	int a;
 //	printf("Enter the number: ");
 //	scanf("%d", &a);
+//	a /= 100;
 //	
-//	a /=  100;
-//	
-//	printf("The number after removing the last two digits: %d", a);
+//	printf("The number after removing the last digit is %d", a);
 
-
+	
 	// 5
 //	int a;
 //	printf("Enter the number: ");
 //	scanf("%d", &a);
-//	
-//	int l = 2 * (a % 10);
-//	
-//	printf("The double of the last digit of the number: %d", l);
-
-	// 6
-//	int a;
-//	printf("Enter the number: ");
-//	scanf("%d", &a);	
+//	a = 2 * (a % 10);
 //
-//	int l = a / 100;
+//	printf("The double of the last digit is %d", a);
+	
+	
+	// 6
+//	int a, l;
+//	printf("Enter the number: ");
+//	scanf("%d", &a);
+//	
+//	l = a / 100;
 //	
 //	int i;
 //	for (i = 0; i < 2; i++) {
@@ -59,106 +60,165 @@ int main() {
 //		a /= 10;
 //	}
 //
-//	printf("The number after swapping the last two digits: %d", l);
+//	printf("The number after swapping the last two digits is %d", l);
+
 
 	// 7
-//	int a, b, c;
-//	printf("Enter the sides (space-separated): ");
-//	scanf("%d %d %d", &a, &b, &c);
+//	float a, b, c, s, area;
+//	printf("Enter the sides separated by spaces: ");
+//	scanf("%f %f %f", &a, &b, &c);
 //	
-//	float s = (a + b + c) / 2;
-//	
-//	float area = sqrt(s * (s - a) * (s - b) * (s - c));
+//	s = (a + b + c) / 2;
+//	area = sqrt(s*(s-a)*(s-b)*(s-c));
 //	
 //	printf("The area of the triangle is %.2f", area);
 
+
 	// 8
-//	int a;
+//	int a, b = 0;
 //	printf("Enter the number: ");
 //	scanf("%d", &a);
 //	
-//	int l = 0;
 //	while (a) {
-//		l = l * 10 + a % 10;
+//		b *= 10;
+//		b += a % 10;
 //		a /= 10;
 //	}
 //	
-//	printf("The number reversed is %d", l);
+//	printf("The value after reversing the integer is %d", b);
+
 
 	// 9
-//	int x1, y1, x2, y2;
-//	printf("Enter the coordinates x1, y1, x2, y2 respectively (space-separated): ");
-//	scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+//	float x_1, y_1, x_2, y_2;
+//	float dist;
 //	
-//	float d = sqrt((x1 - x2 ) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+//	printf("Enter the value of x_1, y_1 (space-separated): ");
+//	scanf("%f %f", &x_1, &y_1);
+//	printf("Enter the value of x_2, y_2 (space-separated): ");
+//	scanf("%f %f", &x_2, &y_2);
 //	
-//	printf("The distance between (%d, %d) and (%d, %d) is %.2f", x1, y1, x2, y2, d);
+//	dist = sqrt(pow((x_1 - x_2), 2) + pow(y_1 - y_2, 2));
+//
+//	printf("The distance between (%.2f, %.2f) and (%.2f, %.2f) is %.2f", x_1, y_1, x_2, y_2, dist);
+	
 	
 	// 10
-//	int s[3];
-//	printf("Enter the sides (space-separated): ");
-//	scanf("%d %d %d", &s[0], &s[1], &s[2]);
+//	int a, b, c;
 //	
-//	int i, j, temp;
-//	for (i = 0; i < 3; i++) {
-//		for (j = i; j < 3; j++) {
-//			if (s[j] < s[i]) {
-//				temp = s[i];
-//				s[i] = s[j];
-//				s[j] = temp;
-//			}
-//		}
-//	}
+//	printf("Enter the sides of the triangle (space-separated): ");
+//	scanf("%d %d %d", &a, &b, &c);
 //	
-//	if (s[2] * s[2] == s[1] * s[1] + s[0] * s[0]) {
-//		printf("The triangle is right-angled.");
+//	if ((a * a == b * b + c * c) ||
+//		(b * b == c * c + a * a) ||
+//		(c * c == a * a + b * b)) {
+//		printf("Triangle with side %d, %d and %d is right-angled.", a, b, c);
 //	}
-	
+
+
 	// 11
 //	int n;
-//	printf("Enter the number of lines: ");
+//	printf("Enter the number of lines to print: ");
 //	scanf("%d", &n);
 //	
-//	int i, j, k;
-//	for (i = 0; i < n; i++) {
-//		for (j = 0; j <= n - i; j++) {
+//	int i, j;
+//	int t = 1;
+//	while (t <= n) {
+//		for (i = 1; i < n - t; i++) {
 //			printf(" ");
 //		}
-//		for (k = 0; k <= i; k++) {
+//		for (j = 1; j <= t; j++) {
 //			printf("* ");
 //		}
 //		printf("\n");
+//		t++;
 //	}
+
 
 	// 12
 	
-//	int arr[3][3] = {
-//						{1, 2, 3}, 
-//						{4, 5, 6}, 
-//						{7, 8, 9}
-//					};
+//	int arr[2][3];
 //	
 //	int i, j;
-//	for (i = 0; i < 3; i++) {
+//	for (i = 0;  i < 2; i++){
+//		for (j = 0; j < 3; j++) {
+//			printf("Enter the value of arr[%d][%d]: ", i, j);
+//			scanf("%d", &arr[i][j]);
+//		}
+//	}
+//	
+//	for (i = 0;  i < 2; i++){
 //		for (j = 0; j < 3; j++) {
 //			printf("%d ", arr[i][j]);
 //		}
 //		printf("\n");
 //	}
-
+	
+	
 	// 13
-	int a, b, c;
-	printf("Enter the sides (space-separated): ");
-	scanf("%d %d %d", &a, &b, &c);
-	
-	int r = sum(a, b, c);
-	
-	printf("The sum of %d, %d, %d is %d", a, b, c, r);
+//	float a, b, c, result;
+//	printf("Enter the value of a, b, c (space-separated): ");
+//	scanf("%f %f %f", &a, &b, &c);
+//	
+//	result = sum(a, b, c);
+//	
+//	printf("The sum of the numbers %.2f, %.2f and %.2f is %.2f",a, b, c, result);
 
+
+	// 14
+//	int a;
+//	printf("Enter the number: ");
+//	scanf("%d", &a);
+//	
+//	if (is_armstrong_num(a)) {
+//		printf("%d is an armstrong number.", a);
+//	}
+//	else {
+//		printf("%d is not an armstrong number.", a);
+//	}
+	
+	
+	// 15
+//	FILE *file;
+//	
+//	char filename[50], ch;
+//	
+//	printf("Enter the filename: ");
+//	scanf("%s", filename);
+//	
+//	file = fopen(filename, "r");
+//	
+//	if (file == NULL) { 
+//		printf("The file can't be opened.");
+//		return 1;
+//	}
+//	
+//	while ((ch = fgetc(file)) != EOF) {
+//		putchar(ch);
+//	}
+//	
+//	fclose(file);
+	
+	
 	
 	return 0;
 }
 
-int sum(a, b, c) {
+float sum(float a, float b, float c) {
 	return a + b + c;
+}
+
+int is_armstrong_num(int a) {
+	int n = a, t = a, d = 0;
+	while (a) {
+		a /= 10;
+		d++;
+	}
+	
+	int s = 0, i;
+	for (i = 0; i < d; i++) {
+		s += pow(n % 10, d);
+		n /= 10;
+	}
+	
+	return (t == s);
 }
